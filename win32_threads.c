79197cea27bc596132d4e55b250a68ad5ef2db76
Ivan Maidanski <ivmai@mail.ru>
2019-09-16 11:39:42 +0300

    Add assertion that thread is suspended when using context registers
    * win32_threads.c (GC_push_stack_for): Add assertion that thread is
    suspended (before using context registers) if thread is not self and
    not blocked.

--- win32_threads.c
+++ win32_threads.c
@@ -1509,6 +1509,7 @@ STATIC word GC_push_stack_for(GC_thread thread, DWORD me)
       /* require looping.                                               */
       word *regs = thread->context_regs;
 
+      GC_ASSERT(thread->suspended);
       sp = thread->context_sp;
 #   else
       /* For unblocked threads call GetThreadContext(). */
@@ -1516,6 +1517,7 @@ STATIC word GC_push_stack_for(GC_thread thread, DWORD me)
       {
         CONTEXT context;
 
+        GC_ASSERT(thread->suspended);
         context.ContextFlags = GET_THREAD_CONTEXT_FLAGS;
         if (!GetThreadContext(THREAD_HANDLE(thread), &context))
           ABORT("GetThreadContext failed");
