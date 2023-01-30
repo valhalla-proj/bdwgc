0a5a8087b2cbf44b99c66a56a4dff2987c3db904
Jonathan Chambers <jonathan@unity3d.com>
2023-01-30 13:53:10 -0500

    Fix link error on bitcode target (case UUM-25113)
    Under the defines Unity provides,
    !defined(DYNAMIC_LOADING) && defined(GC_DONT_REGISTER_MAIN_STATIC_DATA),
    GC_register_data_segments will never be called. However, the
    function was still present and referenced symbols not present when
    targeting bitcode. If the linker didn't remove the dead function,
    linker errors occured. Stub an empty function in this case.

--- os_dep.c
+++ os_dep.c
@@ -2056,6 +2056,9 @@ void GC_register_data_segments(void)
           if ((word)DATASTART < (word)p)
             GC_add_roots_inner(DATASTART, p, FALSE);
         }
+#     elif defined(HOST_ANDROID) && !defined(DYNAMIC_LOADING) && defined(GC_DONT_REGISTER_MAIN_STATIC_DATA)
+        /* avoid even referencing DATASTART & DATAEND as they are       */
+        /* unnecessary and cause linker errors when bitcode is enabled  */
 #     else
         if ((word)DATASTART - 1U >= (word)DATAEND) {
                                 /* Subtract one to check also for NULL  */
